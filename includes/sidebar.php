9bdb45cfec6fc6573cfd2d456bc68c86b4bd4e8a
Rodolfo Hernandez R. <rhernandez@symrecover.cl>
2026-05-28 17:52:12 -0400

    Carga lee pdf completo 20260528

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,10 +1,14 @@
-<nav class="navbar navbar-dark bg-dark flex-column" style="background-color: #236785 !important; min-height: 100vh; width: 250px;">
-    <a class="navbar-brand mx-auto my-3" href="dashboard.php">DemandaAU</a>
+<nav class="nav flex-column">
+    <a class="navbar-brand text-white mb-4" href="/demandaau/vistas/dashboard.php">⚖️ DemandaAU</a>
     <ul class="nav nav-pills flex-column">
-        <li class="nav-item"><a class="nav-link text-white" href="vistas/dashboard.php"><i class="bi bi-grid"></i> Dashboard</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="procesos/importar_excel.php"><i class="bi bi-upload"></i> Importar Excel</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="vistas/listado_demandas.php"><i class="bi bi-list"></i> Demandas</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="/demandaau/vistas/importar_excel.php"><i class="bi bi-upload"></i> Importar Excel</a></li></li>
-        <li class="nav-item"><a class="nav-link text-white" href="/demandaau/procesos/procesar_pdfs.php"><i class="bi bi-file-earmark-pdf"></i> Procesar Pagarés</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="/demandaau/vistas/dashboard.php"><i class="bi bi-speedometer2"></i> Dashboard</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="/demandaau/vistas/importar_excel.php"><i class="bi bi-upload"></i> Importar Excel</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="/demandaau/procesos/procesar_pdf.php"><i class="bi bi-file-earmark-pdf"></i> Leer Pagarés</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="/demandaau/vistas/listado_demandas.php"><i class="bi bi-list"></i> Demandas</a></li>
+        <?php if($_SESSION['rol'] == 'ADMIN'): ?>
+        <li class="nav-item"><a class="nav-link text-white" href="/demandaau/vistas/admin_usuarios.php"><i class="bi bi-people"></i> Usuarios</a></li>
+        <?php endif; ?>
+        <li class="nav-item mt-5"><a class="nav-link text-white" href="/demandaau/procesos/logout.php"><i class="bi bi-box-arrow-right"></i> Salir</a></li>
+        <li class="nav-item"><a class="nav-link text-white" href="/demandaau/procesos/procesar_pdf.php"><i class="bi bi-file-earmark-pdf"></i> Leer Pagarés</a></li>
     </ul>
 </nav>
